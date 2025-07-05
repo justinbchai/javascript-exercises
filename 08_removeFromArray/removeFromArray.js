003904fd2d8a3646f841d76bf81a31033e818fce
Justin Chai <justinbchai@gmail.com>
2025-07-05 15:51:23 -0400

    exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    return arr.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line

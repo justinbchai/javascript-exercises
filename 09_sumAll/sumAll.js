8dd9b2dce564918b6714d881ea6d377af6bffb17
Justin Chai <justinbchai@gmail.com>
2025-07-05 16:04:50 -0400

    exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(low, hi) {
+    if (typeof(low) != 'number' || typeof(hi) != 'number' || !Number.isInteger(low) || !Number.isInteger(hi) || low < 0 || hi < 0) {
+        return 'ERROR';
+    }
+    if (low > hi) {
+        let temp = hi;
+        hi = low;
+        low = temp;
+    }
+    let sum = 0;
+    for (let i = low; i <= hi; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
